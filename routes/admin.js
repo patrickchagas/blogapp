cc6bb23ea7a380152087e7e52da19691d5ff0a66
Patrick Chagas <patrickchagas21@gmail.com>
2019-06-13 19:52:16 -0300

    Editando Categoria

--- routes/admin.js
+++ routes/admin.js
@@ -65,5 +65,45 @@ router.post("/categorias/nova", (req, res) => {
     }
 })
 
+//Editar categoria
+router.get("/categorias/edit/:id", (req, res) => {
+    Categoria.findOne({_id: req.params.id}).then((categoria) => {
+
+        res.render("admin/editcategorias", {categoria: categoria})
+
+    }).catch((error) => {
+
+        req.flash("error_msg", "Essa categoria não existe.")
+        res.redirect("/admin/categorias")
+    })  
+})
+
+router.post("/categorias/edit", (req, res) => {
+
+    Categoria.findOne({_id: req.body.id}).then((categoria) => {
+        
+        categoria.nome = req.body.nome,
+        categoria.slug = req.body.slug
+        
+        categoria.save().then(() =>{
+
+            req.flash("success_msg", "Categoria editada com sucesso!")
+            res.redirect("/admin/categorias")
+
+        }).catch((error) =>{
+
+            req.flash("error_msg", "Houve um erro interno ao salvar a edição da categoria.")
+            res.redirect("/admin/categorias")
+        })
+ 
+    }).catch((error) =>{
+        req.flash("error_msg", "Houve um erro ao editar categoria.")
+        res.redirect("/admin/categorias")
+    })
+
+})
+
+
+
 
 module.exports = router;
