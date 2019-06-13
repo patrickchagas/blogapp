ba27da11bc093ae485a3082fc670da86075d451a
Patrick Chagas <patrickchagas21@gmail.com>
2019-06-13 19:12:44 -0300

    Validação de Formulário

--- routes/admin.js
+++ routes/admin.js
@@ -22,16 +22,39 @@ router.get("/categorias/add", (req, res) => {
 
 //Salvar Categoria no banco
 router.post("/categorias/nova", (req, res) => {
-    const novaCategoria = {
-        nome: req.body.nome,
-        slug: req.body.slug
+
+    var erros = []
+
+    //Validação do formulário
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "Nome inválido"})
     }
 
-    new Categoria(novaCategoria).save().then(() => {
-        console.log("Categoria salva com sucesso!")
-    }).catch((error) => {
-        console.log("Erro ao salvar categoria" + error)
-    })
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.nome == null){
+        erros.push({texto: "Slug inválido"})
+    }
+
+    if(req.body.nome.length < 2){
+        erros.push({texto: "O nome da categoria é muito pequeno."})
+    }
+
+    if(erros.length > 0){
+        res.render("admin/addcategorias", {erros: erros})
+    } else{
+
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+
+        new Categoria(novaCategoria).save().then(() => {
+            req.flash("success_msg", "Categoria criada com sucesso!")
+            res.redirect("/admin/categorias")
+        }).catch((error) => {
+            req.flash("error_msg", "Houve um erro ao salvar a categoria, tenta novamente!")
+            res.redirect("/admin")
+        })
+    }
 })
 
 
